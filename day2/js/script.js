2e71ea06b071abb935392117f54866948346c62b
danielnaon <danielnaon@gmail.com>
2019-06-11 14:55:27 +0300

    first commit

---
+++ day2/js/script.js
@@ -0,0 +1,23 @@
+const maxCapacity = 14;
+let surfTime = true;
+let bestStudent
+const purposeInLife= "loveisgood";
+
+console.log(surfTime)
+console.log(maxCapacity)
+console.log(bestStudent)
+console.log(purposeInLife)
+
+let my = 1
+let result = my + 'is the best thing ever'
+console.log(result)
+
+    let password = 2
+    let confirmPassword = 1
+    console.log(password == confirmPassword)
+
+    let pass = 423 * 12
+    console.log(pass)
+
+    console.log(802 / 2)
+    console.log((5+6)/3)
